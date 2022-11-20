a6b8d6fd85fa11fa62f9cbf3046793fc6ce13db5
oscargutii44 <54816490+oscargutii44@users.noreply.github.com>
2022-11-20 15:01:10 -0600

    formatter
    using prettiter as default code formatter

--- Navigation.js
+++ Navigation.js
@@ -3,9 +3,9 @@ import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 
 //Screen imports
-import { HomeScreen } from './Screens/HomeScreen';
-import { ClientsScreen } from './Screens/ClientsScreen';
-import { ProductsScreen } from './Screens/ProductsScreen';
+import { HomeScreen } from "./Screens/HomeScreen";
+import { ClientsScreen } from "./Screens/ClientsScreen";
+import { ProductsScreen } from "./Screens/ProductsScreen";
 import { SalesScreen } from "./Screens/SalesScreen";
 
 //Auth Screen imports
@@ -13,29 +13,40 @@ import { Login } from "./Screens/Auth/Login";
 import { SignUp } from "./Screens/Auth/SignUp";
 
 //Manupulating Stack Navigation properties
-const HomeStackNav  = createNativeStackNavigator();
+const HomeStackNav = createNativeStackNavigator();
 
 //Creating Stack Navigation Component
 export function HomeStack() {
-    return(
-        <HomeStackNav.Navigator initialRouteName="HomeScreen">
-            <HomeStackNav.Screen name="HomeScreen" component={HomeScreen} ></HomeStackNav.Screen>
-            <HomeStackNav.Screen name="ProductsScreen" component={ProductsScreen}></HomeStackNav.Screen>
-            <HomeStackNav.Screen name="SalesScreen" component={SalesScreen}></HomeStackNav.Screen>
-            <HomeStackNav.Screen name="ClientsScreen" component={ClientsScreen}></HomeStackNav.Screen>
-        </HomeStackNav.Navigator>
-    )
-};
+  return (
+    <HomeStackNav.Navigator initialRouteName="HomeScreen">
+      <HomeStackNav.Screen
+        name="HomeScreen"
+        component={HomeScreen}
+      ></HomeStackNav.Screen>
+      <HomeStackNav.Screen
+        name="ProductsScreen"
+        component={ProductsScreen}
+      ></HomeStackNav.Screen>
+      <HomeStackNav.Screen
+        name="SalesScreen"
+        component={SalesScreen}
+      ></HomeStackNav.Screen>
+      <HomeStackNav.Screen
+        name="ClientsScreen"
+        component={ClientsScreen}
+      ></HomeStackNav.Screen>
+    </HomeStackNav.Navigator>
+  );
+}
 
+// const RootTabs =  createBottomTabNavigator();
 
-  // const RootTabs =  createBottomTabNavigator(); 
-
-  // export function RootNavigation() {
-  //   return (
-  //       <RootTabs.Navigator initialRouteName="HomeScreen" options={{headerShown: false}}>
-  //         <RootTabs.Screen name="HomeScreen" component={HomeStack} options={{headerShown: false}} />
-  //         <RootTabs.Screen name="SettingsScreen" component={SettingsScreen} />
-  //       </RootTabs.Navigator>
-  //   );
-  // }
-  //options={{headerShown: false}}
+// export function RootNavigation() {
+//   return (
+//       <RootTabs.Navigator initialRouteName="HomeScreen" options={{headerShown: false}}>
+//         <RootTabs.Screen name="HomeScreen" component={HomeStack} options={{headerShown: false}} />
+//         <RootTabs.Screen name="SettingsScreen" component={SettingsScreen} />
+//       </RootTabs.Navigator>
+//   );
+// }
+//options={{headerShown: false}}
