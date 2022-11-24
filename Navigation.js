76527aec9ad6c42ec6a43a2e1337f4cb6eef7180
PlinioGR <72715326+PlinioGR@users.noreply.github.com>
2022-11-24 08:47:00 -0600

    Fixing add product navigation

--- Navigation.js
+++ Navigation.js
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useState } from "react";
 
 //Navigation imports
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
@@ -12,7 +12,12 @@ import { SalesScreen } from "./Screens/SalesScreen";
 import { AddOrEditProduct } from "./Screens/Forms/AddOrEditProduct";
 
 //Auth Screen imports
-import { isLogged, isLoggedFalse, isLoggedTrue, HandleLogin } from "./Screens/Auth/Login";
+import {
+  isLogged,
+  isLoggedFalse,
+  isLoggedTrue,
+  HandleLogin,
+} from "./Screens/Auth/Login";
 import { isLoggedSignUp, SignUp } from "./Screens/Auth/SignUp";
 
 //Manupulating Stack Navigation properties
@@ -20,61 +25,82 @@ const HomeStackNav = createNativeStackNavigator();
 
 //Creating Stack Navigation Component
 export function HomeStack() {
-    //SignUp
-    // const isLoggedInSignUp = isLoggedSignUp();
-    // console.log(isLoggedIn);
+  //SignUp
+  // const isLoggedInSignUp = isLoggedSignUp();
+  // console.log(isLoggedIn);
 
-    let isLoggedIn = isLogged();
-    console.log('isloggedin from nav: ', isLoggedIn);
+  let isLoggedIn = isLogged();
+  console.log("isloggedin from nav: ", isLoggedIn);
 
-    // let estado = isLogged();
-    // console.log('estado from login in Nav: ', estado);
+  // let estado = isLogged();
+  // console.log('estado from login in Nav: ', estado);
 
-    //const hola = (estado) => setIsLoggedIn(estado);
-    // console.log('isLoggedIn after useState: ', isLoggedIn)
-    //const isLoggedIn = 'false';
-    const isLoggedInSignUp = 'false';
+  //const hola = (estado) => setIsLoggedIn(estado);
+  // console.log('isLoggedIn after useState: ', isLoggedIn)
+  //const isLoggedIn = 'false';
+  const isLoggedInSignUp = "false";
 
-    // if (isLoggedIn === 'false') {
-    //     setIsLoggedIn('true');
-    // }
-        if (isLoggedIn != 'true' && isLoggedInSignUp === 'false') {
-            return(
-                <HomeStackNav.Navigator initialRouteName="HandleLogin">
-                <HomeStackNav.Screen name="HandleLogin" component={HandleLogin} ></HomeStackNav.Screen>
-                <HomeStackNav.Screen name="SignUp" component={SignUp}></HomeStackNav.Screen>
-            </HomeStackNav.Navigator>
-            )
-        } else {
-            return(
-                <HomeStackNav.Navigator initialRouteName="HomeScreen">
-                    <HomeStackNav.Screen name="HomeScreen" component={HomeScreen} ></HomeStackNav.Screen>
-                    <HomeStackNav.Screen name="ProductsScreen" component={ProductsScreen}></HomeStackNav.Screen>
-                    <HomeStackNav.Screen name="SalesScreen" component={SalesScreen}></HomeStackNav.Screen>
-                    <HomeStackNav.Screen name="ClientsScreen" component={ClientsScreen}></HomeStackNav.Screen>
-                </HomeStackNav.Navigator>
-            )
-        }
+  // if (isLoggedIn === 'false') {
+  //     setIsLoggedIn('true');
+  // }
+  if (isLoggedIn != "true" && isLoggedInSignUp === "false") {
+    return (
+      <HomeStackNav.Navigator initialRouteName="HandleLogin">
+        <HomeStackNav.Screen
+          name="HandleLogin"
+          component={HandleLogin}
+        ></HomeStackNav.Screen>
+        <HomeStackNav.Screen
+          name="SignUp"
+          component={SignUp}
+        ></HomeStackNav.Screen>
+      </HomeStackNav.Navigator>
+    );
+  } else {
+    return (
+      <HomeStackNav.Navigator initialRouteName="HomeScreen">
+        <HomeStackNav.Screen
+          name="HomeScreen"
+          component={HomeScreen}
+        ></HomeStackNav.Screen>
+        <HomeStackNav.Screen
+          name="ProductsScreen"
+          component={ProductsScreen}
+        ></HomeStackNav.Screen>
+        <HomeStackNav.Screen
+          name="SalesScreen"
+          component={SalesScreen}
+        ></HomeStackNav.Screen>
+        <HomeStackNav.Screen
+          name="ClientsScreen"
+          component={ClientsScreen}
+        ></HomeStackNav.Screen>
+        <HomeStackNav.Screen
+          name="AddOrEditProduct"
+          component={AddOrEditProduct}
+        ></HomeStackNav.Screen>
+      </HomeStackNav.Navigator>
+    );
+  }
 
-    // if (isLoggedIn != 'true' && isLoggedInSignUp === 'false') {
-    //     return(
-    //         <HomeStackNav.Navigator initialRouteName="HandleLogin">
-    //         <HomeStackNav.Screen name="HandleLogin" component={HandleLogin} ></HomeStackNav.Screen>
-    //         <HomeStackNav.Screen name="SignUp" component={SignUp}></HomeStackNav.Screen>
-    //     </HomeStackNav.Navigator>
-    //     )
-    // } else {
-    //     return(
-    //         <HomeStackNav.Navigator initialRouteName="HomeScreen">
-    //             <HomeStackNav.Screen name="HomeScreen" component={HomeScreen} ></HomeStackNav.Screen>
-    //             <HomeStackNav.Screen name="ProductsScreen" component={ProductsScreen}></HomeStackNav.Screen>
-    //             <HomeStackNav.Screen name="SalesScreen" component={SalesScreen}></HomeStackNav.Screen>
-    //             <HomeStackNav.Screen name="ClientsScreen" component={ClientsScreen}></HomeStackNav.Screen>
-    //         </HomeStackNav.Navigator>
-    //     )
-    // }
-    
-};
+  // if (isLoggedIn != 'true' && isLoggedInSignUp === 'false') {
+  //     return(
+  //         <HomeStackNav.Navigator initialRouteName="HandleLogin">
+  //         <HomeStackNav.Screen name="HandleLogin" component={HandleLogin} ></HomeStackNav.Screen>
+  //         <HomeStackNav.Screen name="SignUp" component={SignUp}></HomeStackNav.Screen>
+  //     </HomeStackNav.Navigator>
+  //     )
+  // } else {
+  //     return(
+  //         <HomeStackNav.Navigator initialRouteName="HomeScreen">
+  //             <HomeStackNav.Screen name="HomeScreen" component={HomeScreen} ></HomeStackNav.Screen>
+  //             <HomeStackNav.Screen name="ProductsScreen" component={ProductsScreen}></HomeStackNav.Screen>
+  //             <HomeStackNav.Screen name="SalesScreen" component={SalesScreen}></HomeStackNav.Screen>
+  //             <HomeStackNav.Screen name="ClientsScreen" component={ClientsScreen}></HomeStackNav.Screen>
+  //         </HomeStackNav.Navigator>
+  //     )
+  // }
+}
 
 // const RootTabs =  createBottomTabNavigator();
 
